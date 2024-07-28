50c273a01b1354746ae8fa658ec6ece63a7c9d0f
patriciauhh <woahtricia@gmail.com>
2024-07-28 17:11:18 -0400

    scaffolding

---
+++ analysis/evaluate.py
@@ -0,0 +1,21 @@
+import numpy
+import pandas
+import matplot
+
+## descriptive 
+# calculate the mean and median of household income in urban regions 
+# calculate the mean and median # of grocery stores where = 1
+
+
+
+
+
+
+
+
+
+
+
+
+# predictive analysis 
+# linear regression? 
